4211c4218a943f9e532edd63afeddcce0349312a
Peter Persson <peter.persson@infomaker.se>
2022-02-24 09:19:09 +0100

    added logigng

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -4,6 +4,10 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
+console.log('Account: ', process.env.CDK_DEFAULT_ACCOUNT, ' Region: ', process.env.CDK_DEFAULT_REGION)
 new CdkStack(app, 'CdkStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  },
 });
