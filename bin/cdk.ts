0dc4c520a8e9702768a9b5940c8f2cc17d2f069f
Peter Persson <peter.persson@infomaker.se>
2022-02-24 09:56:36 +0100

    added stack decription

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -6,6 +6,7 @@ import { CdkStack } from '../lib/cdk-stack';
 const app = new cdk.App();
 console.log('Account: ', process.env.CDK_DEFAULT_ACCOUNT, ' Region: ', process.env.CDK_DEFAULT_REGION)
 new CdkStack(app, 'CdkStack', {
+  description: 'This is a CDK-test project',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
