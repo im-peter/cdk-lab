de275baa4da59cd3f01d43f550fa90d60aee2756
Peter Persson <peter.persson@infomaker.se>
2022-02-24 16:44:25 +0100

    added acount number to event

--- resources/second.ts
+++ resources/second.ts
@@ -8,25 +8,26 @@ const client = new EventBridge({
     region: 'eu-west-1',
 })
 
-exports.main = async function(event: DynamoDBStreamEvent, context: Context) : Promise<any> {
+exports.main = async function(event: DynamoDBStreamEvent, context: Context): Promise<any> {
     const result = await client.putEvents({
         Entries: [
             {
-              Source: 'dynamodb.stream.lambda',
-              EventBusName: 'cdk-eventbridge-test',
-              DetailType: 'transaction',
-              Time: new Date(),
+                Source: 'dynamodb.stream.lambda',
+                EventBusName: 'cdk-eventbridge-test',
+                DetailType: 'transaction',
+                Time: new Date(),
 
-              Detail: JSON.stringify({
-                event,
-                context
-              })
+                Detail: JSON.stringify({
+                    Account: '034366512379',
+                    event,
+                    context
+                })
             }
         ]
     }).promise()
 
     const body = {
-        Title : 'Event from Dynamo-stream',
+        Title: 'Event from Dynamo-stream',
         context: context,
         event: event,
         eventBridgeResult: result
